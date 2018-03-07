c2e4ea1e800d85f892efef8a662611e06973e4d0
Antonio Ramirez <ajrmzcs@gmail.com>
2018-03-06 23:20:41 -0500

    Changed view calendar order and readme.md image

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center" style="margin-bottom: 1rem">
         <div class="col-sm-3 justify-content-center">
             <a href="{{ route('google-login') }}" class="btn btn-primary">Refresh</a>
@@ -11,20 +11,18 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        @foreach($calendarEvents['2018'] as $key => $events)
-            <div class="col-xs-1">
-                <table class="table table-striped">
+        <div class="col-xs-12 small">
+            <table class="table table-striped">
+                @foreach($calendarEvents['2018'] as $key => $events)
                     <tr>
-                        <th class="text-center">{{ $key }}</th>
-                    </tr>
-                    @foreach($events as $key => $event)
-                        <tr>
+                        <td class="text-center">{{ $key }}</td>
+                        @foreach($events as $key => $event)
                             <td @if(isset($event['color'])) style="background-color: {{ $event['color'] }}" @endif><sup class="text-left align-text-top">{{$key }} </sup><span class="text-center align-middle">{{ isset($event['summary']) ? $event['summary'] : '' }}</span></td>
-                        </tr>
-                    @endforeach
-                </table>
-            </div>
-        @endforeach
+                        @endforeach
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
 </div>
 @endsection
